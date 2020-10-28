d350332c5c978efb728cdb3a893007981632918c
soner <soner@linxa.com>
2020-10-28 19:04:34 +0300

    Minor fix

--- src/main/java/com/bestseller/ecommerce/config/DefaultExceptionHandler.java
+++ src/main/java/com/bestseller/ecommerce/config/DefaultExceptionHandler.java
@@ -5,11 +5,9 @@ import com.bestseller.ecommerce.exception.ProductNotFoundException;
 import com.bestseller.ecommerce.exception.UserAlreadyExistsException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -63,6 +61,6 @@ public class DefaultExceptionHandler {
 	@ExceptionHandler(value = Exception.class)
 	public ResponseEntity<Object> handleException(Exception e) {
 		logger.error(e.getMessage());
-		return new ResponseEntity<>("There is an unexpected error.", HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<>("There is an unexpected error.", HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
