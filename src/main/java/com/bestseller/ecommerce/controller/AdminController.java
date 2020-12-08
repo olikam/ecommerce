56e369cd416510b766495cc5713063256a0fd7f5
soner <soner@linxa.com>
2020-12-08 10:46:04 +0300

    PATCH to PUT

--- src/main/java/com/bestseller/ecommerce/controller/AdminController.java
+++ src/main/java/com/bestseller/ecommerce/controller/AdminController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,7 +46,7 @@ public class AdminController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @PatchMapping("/product")
+    @PutMapping("/product")
     public ResponseEntity<String> update(@Valid @RequestBody ProductUpdateRequest productUpdateRequest) {
         productService.update(productUpdateRequest);
         return new ResponseEntity<>(HttpStatus.OK);
